53ef6fb9b12357cea29c8d5c28e7a88adfaf07c5
Guillermo Teijeiro <gteijeiro@outlook.com>
2023-10-19 11:48:26 +0000

    ex 03 02

--- excersices/ex_03_02/ex_03_02.py
+++ excersices/ex_03_02/ex_03_02.py
@@ -1,15 +1,17 @@
 sh = input('Enter Hours: ')
 sr = input('Enter Rate: ')
-fh = float(sh)
-fr = float(sr)
-# print(fh, fr)
+try:
+    fh = float(sh)
+    fr = float(sr)
+except:
+    print('Error, please enter numeric input')
+    quit()
+    x
+print(fh, fr)
 if fh > 40 :
-    print('Overtime')
     reg = fr * fh
     otp = (fh - 40.0) * (fr * 0.5)
-    print(reg, otp)
     xp = reg + otp
 else:
-    print('Regular')
     xp = fh * fr
 print('Pay:', xp)
