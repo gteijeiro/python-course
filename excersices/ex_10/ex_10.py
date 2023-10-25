721bca1dd0e848ab50db167d0d967f03a1fcd407
Guillermo Teijeiro <gteijeiro@outlook.com>
2023-10-25 23:25:37 +0000

    ex 09 ex 10

---
+++ excersices/ex_10/ex_10.py
@@ -0,0 +1,35 @@
+fname = input('Enter File: ')
+if len(fname) < 1 : fname = 'clown.txt'
+hand = open(fname)
+
+di = dict()
+for lin in hand:
+    lin = lin.rstrip()
+    wds = lin.split()
+    for w in wds:
+        # idiom: retrieve/create/update counter        
+        di[w] = di.get(w, 0) + 1
+# print(di)
+
+# now we want to find the most common word
+largest = -1
+theword = None
+for k,v in di.items() :
+    if v > largest :
+        largest = v
+        theword = k # capture/remember the key that was largest
+ 
+# print(di)
+
+tmp = list()
+for k,v in di.items() :
+    newt = (v,k)
+    tmp.append(newt)
+
+# print('Flipped', tmp)
+
+tmp = sorted(tmp, reverse=True)
+# print('Sorted', tmp)
+
+for v,k in tmp[:5] :
+    print(k,v)
